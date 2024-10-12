cdb65d0b5af9ba2dc71a73e14cd9ef1e6d4c5b99
saransrini03 <saransrini003@gmail.com>
2024-10-12 19:48:25 +0530

    uichange16

--- components/ConfirmationComponent.js
+++ components/ConfirmationComponent.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from 'react';
 import "tailwindcss/tailwind.css";
 
@@ -8,34 +8,31 @@ const ConfirmationComponent = () => {
   const handleResponse = (response) => {
     setSubmitted(response);
     if (response === 'yes') {
-      // Redirect to the specified URL
       window.location.href = "https://forms.gle/ohEG2zF6mvJnoUyDA";
-    }
-    else{
-        window.location.href = "https://forms.gle/Q8hGhw5RkGpwwj949";
+    } else {
+      window.location.href = "https://forms.gle/Q8hGhw5RkGpwwj949";
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center p-8 bg-white shadow-lg rounded-lg max-w-md mx-auto">
-      <h2 className="text-2xl font-semibold mb-6 text-gray-800">
+    <div className="flex flex-col items-center justify-center p-6 sm:p-8 bg-white shadow-lg rounded-lg max-w-md mx-auto">
+      <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-gray-800 text-center">
         Did you submit your paper and receive an acceptance mail?
       </h2>
-      <div className="flex space-x-4 mb-4">
+      <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 mb-4">
         <button
-          className="bg-blue-600 text-white border px-20 py-3 rounded-lg shadow hover:bg-blue-700 transition duration-200"
+          className="bg-blue-600 text-white border px-16 py-3 rounded-lg shadow hover:bg-blue-700 transition duration-200 w-full sm:w-auto"
           onClick={() => handleResponse('yes')}
         >
           Yes
         </button>
         <button
-          className="bg-white text-black border border-black px-20 py-3 rounded-lg shadow hover:bg-red-700 transition duration-200"
+          className="bg-white text-black border border-black px-16 py-3 rounded-lg shadow hover:bg-red-700 transition duration-200 w-full sm:w-auto"
           onClick={() => handleResponse('no')}
         >
           No
         </button>
       </div>
-      
     </div>
   );
 };
